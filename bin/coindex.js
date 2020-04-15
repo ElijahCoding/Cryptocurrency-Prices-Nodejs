882eea4d5c47a45b422d37e506bad389b49a0b1f
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-15 14:10:10 +0300

    coindex key commands

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const program = require('commander')
+const pkg = require('../package.json')
 
 program
-    .version('1.0.0')
+    .version(pkg.version)
+    .command('key', 'Manage API Key -- https://nomics.com')
+    .command('check', 'Check Coin Price Info')
     .parse(process.argv)
